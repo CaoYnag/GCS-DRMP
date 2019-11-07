862148ca382440d602387118cec2e5ac4699b23c
CaoYang <CaoYangCS@outlook.com>
2019-11-07 16:20:14 +0800

    added model list, and modified some styles.

--- src/component/om/ModelList.vue
+++ src/component/om/ModelList.vue
@@ -1,147 +1,119 @@
 <template>
-  <div class="region container">
-    <div class="region carousel">
-      <Carousel
-        v-model="value3"
-        :autoplay="setting.autoplay"
-        :autoplay-speed="setting.autoplaySpeed"
-        :dots="setting.dots"
-        :radius-dot="setting.radiusDot"
-        :trigger="setting.trigger"
-        :arrow="setting.arrow" loop>
-        <CarouselItem>
-          <div class="region carousel item">
-            <div style="background-image: url('/static/sv/rg_preview_big1.png')">
-              <div class="title">拙政园</div>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div class="region carousel item">
-            <div style="background-image: url('/static/sv/rg_preview_big2.png')">
-              <div class="title">狮子林</div>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div class="region carousel item">
-            <div style="background-image: url('/static/sv/rg_preview_big3.png')">
-              <div class="title">虎丘</div>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div class="region carousel item">
-            <div style="background-image: url('/static/sv/rg_preview_big1.png')">
-              <div class="title">金鸡湖</div>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div class="region carousel item">
-            <div style="background-image: url('/static/sv/rg_preview_big2.png')">
-              <div class="title">独墅湖</div>
-            </div>
-          </div>
-        </CarouselItem>
-      </Carousel>
-    </div>
-    <Divider><h3>热门地区</h3></Divider>
-    <div ref="rglist" class="region list">
-      <div v-if="items && items.length > 0">
-        <vk-grid>
-          <div v-for="(it) in items">
-            <div class="model item">
-                <div class="title">{{it.name}}</div>
-            </div>
-          </div>
-        </vk-grid>
-      </div>
-      <div v-else-if="items">
-        <div>
-          <h3>Empty</h3>
-        </div>
-      </div>
-      <div v-else>loading</div>
-    </div>
-  </div>
+<div class="model container">
+    <Divider>
+        <h3>模型列表</h3>
+    </Divider>
+    <Table stripe :columns="columns" :data="datas"></Table>
+</div>
 </template>
+
 <style>
-  .container {
+.container {
     width: 100%;
     height: 100%;
-  }
-
-  .region.item {
-    text-align: center;
-  }
-
-  .region.item img {
-    width: 100px;
-    height: 75px;
-  }
-
-  .region.carousel {
-    width: 100%;
-    height: 256px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  .region.carousel.item {
-    width: 100%;
-  }
-
-  .region.carousel.item > div {
-    width: 100%;
-    height: 256px;
-    background-clip: content-box;
-    background-size: 100% auto
-  }
-
-  .region.carousel.item .title {
-    height: 24px;
-    line-height: 24px;
-    font-size: 24px;
-    text-align: left;
-    background-color: #40404040;
-    color: white;
-    bottom: 0;
-    position: absolute;
-    width: 100%;
-  }
+}
 </style>
-<script>
-  import axios from "axios";
 
-  export default {
+<script>
+import axios from "axios";
+
+export default {
     data: () => ({
-      items: null,
-      value3: 0,
-      setting: {
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: 'inside',
-        radiusDot: true,
-        trigger: 'click',
-        arrow: 'always'
-      }
+        columns: [{
+            title: 'Name',
+            key: 'name'
+        }, {
+            title: 'Type',
+            key: 'type'
+        }, {
+            title: 'Pos',
+            key: 'pos'
+        }, {
+            title: 'Scl',
+            key: 'scl'
+        }, {
+            title: 'Rot',
+            key: 'rot'
+        }, {
+            title: 'Desc',
+            key: 'desc'
+        }, {
+            title: 'Time',
+            key: 'time'
+        }, {
+            title: 'Worker',
+            key: 'worker'
+        }],
+        items: [],
+        datas: []
     }),
     created() {
-      this.$nextTick(() => {
-        this.init()
-      })
+        this.$nextTick(() => {
+            this.init()
+        })
     },
     methods: {
-      init() {
-        this.items = [{"uid":"dGVtcG9ialdlZCBBdWcgMjggMTU6NTc6NTMgQ1NUIDIwMTk=","name":"temp","type":"obj","desc":"temp\t\t\t\t--gen by oblique services.","lng":0.0,"lat":0.0,"alt":0.0,"scaleX":0.0,"scaleY":0.0,"scaleZ":0.0,"yaw":0.0,"pitch":0.0,"roll":0.0,"timestamp":1566983668000,"upload":1566983668000,"worker":null},{"uid":"VGVzdHVua25vd25UaHUgQXVnIDI5IDE2OjE5OjU1IENTVCAyMDE5","name":"Test","type":"unknown","desc":"Test\t\t\t\t--gen by oblique services.","lng":0.0,"lat":0.0,"alt":0.0,"scaleX":0.0,"scaleY":0.0,"scaleZ":0.0,"yaw":0.0,"pitch":0.0,"roll":0.0,"timestamp":1567066795000,"upload":1567066795000,"worker":null}];
-        // axios.get("http://localhost:9094/api/list").then((rsp) => {
-        //   this.items = rsp.data.data;
-        // }).catch(err => {
-        //   console.log(err);
-        // });
-        console.log(this.items);
-      }
-    }
-  }
+        init() {
+            this.items = [{
+                "uid": "dGVtcG9ialdlZCBBdWcgMjggMTU6NTc6NTMgQ1NUIDIwMTk=",
+                "name": "temp",
+                "type": "obj",
+                "desc": "temp\t\t\t\t--gen by oblique services.",
+                "lng": 0.0,
+                "lat": 0.0,
+                "alt": 0.0,
+                "scaleX": 0.0,
+                "scaleY": 0.0,
+                "scaleZ": 0.0,
+                "yaw": 0.0,
+                "pitch": 0.0,
+                "roll": 0.0,
+                "timestamp": 1566983668000,
+                "upload": 1566983668000,
+                "worker": null
+            }, {
+                "uid": "VGVzdHVua25vd25UaHUgQXVnIDI5IDE2OjE5OjU1IENTVCAyMDE5",
+                "name": "Test",
+                "type": "unknown",
+                "desc": "Test\t\t\t\t--gen by oblique services.",
+                "lng": 0.0,
+                "lat": 0.0,
+                "alt": 0.0,
+                "scaleX": 0.0,
+                "scaleY": 0.0,
+                "scaleZ": 0.0,
+                "yaw": 0.0,
+                "pitch": 0.0,
+                "roll": 0.0,
+                "timestamp": 1567066795000,
+                "upload": 1567066795000,
+                "worker": null
+            }];
+            // axios.get("http://localhost:9094/api/list").then((rsp) => {
+            //   this.items = rsp.data.data;
+            // }).catch(err => {
+            //   console.log(err);
+            // });
+            console.log(this.items);
+            this.formatData();
+        },
+        formatData() {
+            this.datas = [];
+            for (var item of this.items) {
+                var data = {};
+                data.name = item.name;
+                data.type = item.type;
+                data.desc = item.desc;
+                data.worker = item.worker === null || item.worker === '' ? 'unkonwn' : item.worker;
+                data.pos = item.lng + ", " + item.lat + ", " + item.alt;
+                data.scl = item.scaleX + ", " + item.scaleY + ", " + item.scaleZ;
+                data.rot = item.yaw + ", " + item.pitch + ", " + item.roll;
+                let date = new Date(item.timestamp);
+                data.time = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+                this.datas.push(data);
+            }
+        }
+    },
+
+}
 </script>
